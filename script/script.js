2f30026f8ae6a35795fa1eb1eedadf4709f5e7bb
frankii -- <franjseco@gmail.com>
2021-01-27 14:54:58 +0100

    Translation

---
+++ script/script.js
@@ -0,0 +1,23 @@
+// function underli() {
+//   let es = document.getElementById('link-es');
+//   es.classList.remove('header__lang-link_active');
+//   let en = document.getElementById('link-en');
+//   en.classList.add('header__lang-link_active');
+// }
+
+// function underli_es() {
+//   let en2 = document.getElementById('link-en');
+//   en2.classList.remove('header__lang-link_active');
+//   let es2 = document.getElementById('link-es');
+//   es2.classList.add('header__lang-link_active');
+// }
+
+function underline() {
+  console.log("Hola");
+}
+
+
+
+document.getElementById('link-en').addEventListener(onclick, underline());
+
+
